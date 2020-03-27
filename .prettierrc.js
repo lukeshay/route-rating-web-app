dfc5d2fab8cf3a41ce8b969f89ca39895f12d32a
LukeShay <shay.luke17@gmail.com>
2020-03-27 13:40:15 -0500

    Fix issues.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,8 @@
-module.exports = require('@lukeshay/prettier-config');
+module.exports = {
+  "trailingComma": "es5",
+  "tabWidth": 2,
+  "arrowParens": "always",
+  "jsxSingleQuote": false,
+  "singleQuote": true,
+  "semi": true,
+}
